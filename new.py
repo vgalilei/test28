5dd637f48a3e1bd64da8f4d52b07f07f906f8d83
galilei <galilei.mail@gmail.com>
2020-07-28 19:58:05 +0300

    Implement my super cool feature

--- new.py
+++ new.py
@@ -2,5 +2,10 @@
 
 class MySuperClass(object):
 
-    def __init__():
+    def __init__(self):
 	    self._o = None
+        
+        
+    def my_super_feature(self):
+        # A lot of usefull code for perfect feature
+        print("sdfasdfasdf") 
